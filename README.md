2c8af10c6cb7175b5db5a9c1bc0b6e7e6e628587
wk <somnuk.wk@gmail.com>
2018-03-27 16:18:20 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## .NET Core + Oracle Database
+
+- [ODP.NET Core Introduction](https://www.youtube.com/watch?v=tGXs3VmflQU)
+- [Oracle Data Provider for .NET Core 12.2 Beta 2](http://www.oracle.com/technetwork/topics/dotnet/downloads/odpnetcorebeta-4077982.html)
+- [Oracle .NET Code Sample Respository](https://github.com/oracle/dotnet-db-samples/tree/master/samples/dotnet-core)
---
+++ src/DotNetOracle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace DotNetOracle {
+    class Program {
+        static void Main(string[] args) {
+            var connectionString = "User Id=system;Password=oracle;Data Source=localhost:49161/xe";
+
+            using (var connection = new OracleConnection(connectionString))
+            using (var command = connection.CreateCommand()) {
+                command.CommandText = "SELECT * FROM Help";
+
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+                while (reader.Read()) {
+                    Console.WriteLine("Info: " + reader.GetString(0));
+                }
+            }
+        }
+    }
+}
